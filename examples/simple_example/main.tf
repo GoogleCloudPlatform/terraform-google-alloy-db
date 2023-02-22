ed3837efdc7dce25fc89b341be50660cf471476a
haprcr <124026578+haprcr@users.noreply.github.com>
2023-02-22 11:45:34 +0530

    adding network resources

--- examples/simple_example/main.tf
+++ examples/simple_example/main.tf
@@ -1,22 +1,57 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+provider "google-beta" {
+}
 
-module "alloy_db" {
-  source = "../.."
+module "alloy-db"{
+    source = "../.."
+    cluster = {
+        cluster_id = "alloydb-v-4-cluster", 
+        location = "us-central1"
+    }
+    network_self_link = "projects/${var.project_id}/global/networks/${var.network_name}"
+    automated_backup_policy = {
+        backup_window ="1800s", 
+        enabled =true, 
+        days_of_week =["FRIDAY"], 
+        start_times ={"hours"=2,"minutes"=0,"seconds"=0,"nanos"=0}, 
+        quantity_based_retention ={
+            count = 1
+        }
+    }
+    
+    primary_instance = {
+        instance_id = "prim-instance-1", 
+        instance_type = "PRIMARY", 
+        machine_config = {
+            cpu_count=2
+        }
+    }
 
-  project_id  = var.project_id
-  bucket_name = var.bucket_name
+    read_pool_instance = {
+        instance_id = "read-instance-1", 
+        instance_type = "READ_POOL", 
+        read_pool_config = {
+            node_count = 2
+        }
+    }
+
+    depends_on = [google_compute_network.default, google_compute_global_address.private_ip_alloc, google_service_networking_connection.vpc_connection]
+}
+
+resource "google_compute_network" "default" {
+        name     = var.network_name
+}
+
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "adb-v2"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
