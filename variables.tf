171b7acbcbb2b7f513f405e741e6bc1b4ee621cf
haprcr <124026578+haprcr@users.noreply.github.com>
2023-02-22 11:48:11 +0530

    decluttered the variables

--- variables.tf
+++ variables.tf
@@ -1,44 +1,48 @@
+variable "network_self_link" {
+    type = string
+}
+
 variable "cluster" {
   type = object({
     cluster_id    = string,
     location      = string,
-    backup_window = string,
-    enabled       = bool
-    }),
-    quantity_based_retention = object({
-      count = number
-    }),
   })
 }
 
-
 variable "automated_backup_policy"{
-  type = object({
+    type = object({
+      backup_window = string,
+      enabled = bool,
       days_of_week = list(string),
       start_times = object({
         hours   = number,
         minutes = number,
         seconds = number,
         nanos   = number
-      })
+      }),
+      quantity_based_retention = object({
+        count = number
+      }),
+    })
 }
-variable "prim_instance" {
-  type = list(object({
+
+variable "primary_instance" {
+  type = object({
     instance_id   = string,
     instance_type = string,
     machine_config = object({
       cpu_count = number,
     }),
-  }))
+  })
 }
 
 variable "read_pool_instance" {
-  type = list(object({
+  type = object({
     instance_id   = string,
     instance_type = string,
     read_pool_config = object({
       node_count = number,
     }),
-  }))
+  })
 
 }
