c5fd94408e7930b75f895a5c99ae2456e0b32712
haprcr <124026578+haprcr@users.noreply.github.com>
2023-02-27 12:30:22 +0530

    mapped the variables accordingly to the PRD

--- variables.tf
+++ variables.tf
@@ -3,36 +3,51 @@ variable "network_self_link" {
     type = string
 }
 
-variable "cluster" {
+variable "cluster_id" {
   description = "Configuration of the AlloyDb cluster."
-  type = object({
-    cluster_id    = string,
-    location      = string,
-  })
+  type    = string
   validation{
-    condition = can(regex("^[a-z0-9-]+$", var.cluster.cluster_id))
+    condition = can(regex("^[a-z0-9-]+$", var.cluster_id))
     error_message = "ERROR: Cluster ID must contain only Letters(lowercase), number, and hyphen"
   }
+}
+
+variable "cluster_location" {
+  description = "Location where AlloyDb cluster will be deployed."
+  type    = string
+  default = "us-central1"
+}
+
+variable "cluster_labels" {
+  type    = map(string)
+  default = {}
+}
+
+variable "cluster_display_name" {
+  type = string
+  default = ""
+}
+
+variable "cluster_initial_user" {
+    type = object({
+        user = optional(string),
+        password = string
+    })
 }
 
 variable "automated_backup_policy"{
     description = "The automated backup policy for this cluster."
     type = object({
-      backup_window = string,
-      enabled = bool,
-      days_of_week = list(string),
-      start_times = object({
-        hours   = number,
-        minutes = number,
-        seconds = number,
-        nanos   = number
+      location = optional(string),
+      backup_window = optional(string),
+      enabled = optional(bool),
+      weekly_schedule = object({
+            days_of_week = optional(list(string)),
+            start_times = list(string),
       }),
-      quantity_based_retention = object({
-        count = number
-      }),
-      time_based_retention = optional(object({
-          retention_period = string
-      }))
+      quantity_based_retention_count = optional(number),
+      time_based_retention_count = optional(string),
+     labels = optional(map(string))
     })
     validation {
         condition = can(regex("^(true|false)$",var.automated_backup_policy.enabled))
@@ -40,17 +55,24 @@ variable "automated_backup_policy"{
     }
 }
 
+variable "instance_iam" {
+  description = "The list of permissions on alloydb instance"
+  type        = list(string)
+  default     = []
+}
+
+
 variable "primary_instance" {
   description = "Primary cluster configuration that supports read and write operations."
   type = object({
     instance_id   = string,
     instance_type = string,
-    machine_config = object({
-      cpu_count = number,
-    }),
+    machine_cpu_count = number,
+    display_name = string,
+    database_flags = map(string)
   })
   validation {
-      condition = can(regex("^(2|4|8|16|32|64)$", var.primary_instance.machine_config.cpu_count))
+      condition = can(regex("^(2|4|8|16|32|64)$", var.primary_instance.machine_cpu_count))
       error_message = "cpu count must be one of [2 4 8 16 32 64]"
   }
   validation{
@@ -60,15 +82,14 @@ variable "primary_instance" {
 }
 
 variable "read_pool_instance" {
-  type = object({
+  type = list(object({
     instance_id   = string,
+    display_name = string,
     instance_type = string,
-    read_pool_config = object({
-      node_count = number,
-    }),
-  })
-  validation{
-      condition = can(regex("^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$", var.read_pool_instance.instance_id))
-      error_message = "Read Pool Instance ID should satisfy the following pattern ^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$"
-  }
+    node_count = number,
+    database_flags = map(string),
+    availability_type = string,
+    ZONE = string,
+    machine_cpu_count = number
+  }))
 }
