024303393ff36607aade11410bbdc7d4ae9eac76
haprcr <124026578+haprcr@users.noreply.github.com>
2023-03-09 17:08:42 +0530

    Create primary_instance_adb_cluster.go

---
+++ test/integration/example_with_primary/primary_instance_adb_cluster.go
@@ -0,0 +1,32 @@
+package primary_instance_adb_cluster
+
+import (
+	"strings"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+)
+
+func TestExampleWithPrimary(t *testing.T) {
+	example := tft.NewTFBlueprintTest(t)
+
+	example.DefineVerify(func(assert *assert.Assertions) {
+		projectID := example.GetStringOutput("project_id")
+		alloydb_cluster_id_path := strings.Split(example.GetStringOutput("cluster_id"), "/")
+		alloydb_primary_instance_path := strings.Split(example.GetStringOutput("primary_instance_id"), "/")
+
+		alloydb_cluster_id := alloydb_cluster_id_path[len(alloydb_cluster_id_path)-1]
+		alloydb_primary_instance_id := alloydb_primary_instance_path[len(alloydb_primary_instance_path)-1]
+
+
+		expected_projectID := "alloydb-dev-36c1"
+		expected_clusterID := "alloydb-cluster-with-prim"
+		expected_primaryInstanceID := "primary-instance-1"
+
+		assert.Equal(expected_projectID, projectID)
+		assert.Equal(expected_clusterID, alloydb_cluster_id)
+		assert.Equal(expected_primaryInstanceID, alloydb_primary_instance_id)
+	})
+
+	example.Test()
+}
