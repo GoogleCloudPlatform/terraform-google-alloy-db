7d5017a4ba2c27eaf0e51b11824f1dc2af770662
Imran Nayer <imrannayer@google.com>
2023-04-03 18:16:44 -0500

    fixed integration test

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+// Copyright 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -90,13 +90,6 @@ variable "automated_backup_policy" {
     }
   }
 }
-
-# variable "instance_iam" {
-#   description = "The list of permissions on alloydb instance"
-#   type        = list(string)
-#   default     = []
-# }
-
 
 variable "primary_instance" {
   description = "Primary cluster configuration that supports read and write operations."
