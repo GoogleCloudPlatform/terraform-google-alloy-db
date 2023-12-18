9aea0db12dad05a15f5d64049a11c6e579973e4f
bharathkkb <bharathkrishnakb@gmail.com>
2023-12-18 20:19:23 +0000

    chore: update examples to use registry

--- examples/simple_example/cross_region_replica.tf
+++ examples/simple_example/cross_region_replica.tf
@@ -15,7 +15,9 @@
  */
 
 module "alloy-db-secondary" {
-  source           = "../.."
+  source  = "GoogleCloudPlatform/alloy-db/google"
+  version = "~> 2.0"
+
   cluster_id       = "alloydb-v6-cluster-secondary"
   cluster_location = var.secondary_region
   project_id       = var.project_id
