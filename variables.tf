328729820f67a0e51d04ba3a4a0ea7f50e0d61e7
Austin Sabel <45413@users.noreply.github.com>
2024-03-26 10:36:17 -0400

    feat: add support for query insights support (#71)
    Co-authored-by: Imran Nayer <imrannayer@google.com>

--- variables.tf
+++ variables.tf
@@ -112,10 +112,17 @@ variable "primary_instance" {
     annotations        = optional(map(string))
     gce_zone           = optional(string)
     availability_type  = optional(string)
-    machine_cpu_count  = optional(number, 2),
+    machine_cpu_count  = optional(number, 2)
     ssl_mode           = optional(string)
-    require_connectors = optional(bool),
+    require_connectors = optional(bool)
+    query_insights_config = optional(object({
+      query_string_length     = optional(number)
+      record_application_tags = optional(bool)
+      record_client_address   = optional(bool)
+      query_plans_per_minute  = optional(number)
+    }))
   })
+
   validation {
     condition     = can(regex("^(2|4|8|16|32|64|96|128)$", var.primary_instance.machine_cpu_count))
     error_message = "machine_cpu_count must be one of [2, 4, 8, 16, 32, 64, 96, 128]"
@@ -123,6 +130,22 @@ variable "primary_instance" {
   validation {
     condition     = can(regex("^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$", var.primary_instance.instance_id))
     error_message = "Primary Instance ID should satisfy the following pattern ^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$"
+  }
+
+  validation {
+    condition = var.primary_instance.query_insights_config == null || (
+      try(var.primary_instance.query_insights_config.query_string_length, 0) >= 256 &&
+      try(var.primary_instance.query_insights_config.query_string_length, 0) <= 4500
+    )
+    error_message = "Query string length must be between 256 and 4500. The default value is 1024."
+  }
+
+  validation {
+    condition = var.primary_instance.query_insights_config == null || (
+      try(var.primary_instance.query_insights_config.query_plans_per_minute, 0) >= 0 &&
+      try(var.primary_instance.query_insights_config.query_plans_per_minute, 0) <= 20
+    )
+    error_message = "Query plans per minute must be between 0 and 20. The default value is 5."
   }
 }
 
@@ -135,9 +158,15 @@ variable "read_pool_instance" {
     database_flags     = optional(map(string))
     availability_type  = optional(string)
     gce_zone           = optional(string)
-    machine_cpu_count  = optional(number, 2),
+    machine_cpu_count  = optional(number, 2)
     ssl_mode           = optional(string)
-    require_connectors = optional(bool),
+    require_connectors = optional(bool)
+    query_insights_config = optional(object({
+      query_string_length     = optional(number)
+      record_application_tags = optional(bool)
+      record_client_address   = optional(bool)
+      query_plans_per_minute  = optional(number)
+    }))
   }))
   default = []
   validation {
