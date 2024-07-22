7a61632d0e1c68eb2200595d77980e9bc0858eee
Rayane Bellazaar <rayane.bellazaar@gmail.com>
2024-07-22 17:46:38 +0200

    enh: apply the requested changes

--- main.tf
+++ main.tf
@@ -159,9 +159,9 @@ resource "google_alloydb_instance" "primary" {
     content {
       enable_public_ip = var.primary_instance.enable_public_ip
       dynamic "authorized_external_networks" {
-        for_each = var.primary_instance.cidr_range == null ? [] : ["authorized_external_networks"]
+        for_each = var.primary_instance.cidr_range == null ? [] : toset(var.primary_instance.cidr_range)
         content {
-          cidr_range = join(",", var.primary_instance.cidr_range)
+          cidr_range = authorized_external_networks.value
         }
       }
     }
@@ -223,7 +223,7 @@ resource "google_alloydb_instance" "read_pool" {
       enable_public_ip = each.value.enable_public_ip
 
       dynamic "authorized_external_networks" {
-        for_each = each.value.cidr_range != null ? each.value.cidr_range : []
+        for_each = each.value.cidr_range == null ? [] : toset(each.value.cidr_range)
         content {
           cidr_range = authorized_external_networks.value
         }
